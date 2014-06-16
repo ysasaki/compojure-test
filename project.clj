c6736281862f7aaf367d01537c328b0f456992b6
Yoshihiro Sasaki <yosasaki@reado.jp>
2014-06-16 21:53:32 +0900

    Updates project.clj

--- project.clj
+++ project.clj
@@ -5,10 +5,10 @@
                  [compojure "1.1.6"]
                  [ring/ring-jetty-adapter "1.3.0"]]
   :plugins [[lein-ring "0.8.10"]]
+  :min-lein-version "2.0.0"
   :uberjar-name "heroku-test-standalone.jar"
-  :main heroku-test.handler
-  :aot [heroku-test.handler]
   :ring {:handler heroku-test.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring-mock "0.1.5"]]}})
+                        [ring-mock "0.1.5"]]}
+   :uberjar {:main heroku-test.handler, :aot :all}})
